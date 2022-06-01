1138467af2bee23271755541b32faa2d9b01762c
calgil <clgilbert1105@gmail.com>
2022-06-01 15:22:39 -0600

    created modals

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
+const isVisible = 'is-visible';
+const hidden = 'hidden';
 
 const select = '.state-select';
 const parksContainer = '.main-parks';
 const park = '.park-card';
 const button = 'button';
+
+const modal = '.modal-container';
+const modalBg = '.park-modal';
+const modalClose = '[data-close]';
+
+
+
 const heart = 'fa-heart';
 const favorite = 'favorite';
+const favButton = '[data-favorite]';
+const favoriteContainer = '.favorite-container';
+const favoriteHeader = '.fav-header';
 
 const stateSelect = document.querySelector(select);
 const mainContainer = document.querySelector(parksContainer);
+const modalContainer = document.querySelector(modal);
+
+const favOpen = document.querySelector(favButton);
+const favContainer = document.querySelector(favoriteContainer);
+const favHeader = document.querySelector(favoriteHeader);
+const favClose = document.querySelector('.close');
 
 let parks = [];
+let favParks = [];
+
+
+
+favClose.addEventListener('click', () => {
+    [favContainer, favHeader].map((elm) => elm.classList.remove(isVisible));
+    [favContainer, favHeader].map((elm) => elm.classList.add(hidden));
+})
+
+favOpen.addEventListener('click', () => {
+    [favContainer, favHeader].map((elm) => elm.classList.add(isVisible));
+    [favContainer, favHeader].map((elm) => elm.classList.remove(hidden));
+})
 
 const findPark = (parkId, array) => {
     const park = array.find((park) => park.id === parkId);
@@ -18,7 +49,18 @@ const findPark = (parkId, array) => {
 
 const addToFavorites = (parkId) => {
     const park = findPark(parkId, parks);
-    console.log(park);
+    // I want to push these parks to another array, assuming that array does not already have have this park
+    // const parkIdentity = park.id += 'fav';
+    // console.log('add', findPark(park.id, favParks));
+    // const duplicate = findPark(park.id, favParks) 
+    // duplicate === undefined
+    findPark(parkIdentity, favParks) === undefined
+    ? favParks.push(park)
+    : console.log('duplicate');
+
+    // favParks.map((park) => park.id += 'fav');
+
+    console.log('favs', favParks);
 }
 
 const handleFavorite = (elm) => {
@@ -35,19 +77,57 @@ const getFavoriteData = (elm) => {
     : handleFavorite(elm)
 }
 
+const closeModal = () => {
+    document.addEventListener('keyup', (e) => {
+        if(e.key === 'Escape'){
+            modalContainer.innerHTML = '';
+        }
+    });
+    document.addEventListener('click', (e) => {
+        if(e.target === document.querySelector(modalBg)){
+            modalContainer.innerHTML = '';
+        }
+    })
+    const closeModalButton = document.querySelector(modalClose);
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.innerHTML = '';
+    });
+};
+
+const buildModal = (park) => {
+    const {fullName, description} = park;
+    const {altText, url} = park.images[1];
+    const parkModal = document.createElement('div');
+    parkModal.classList.add('park-modal');
+    parkModal.innerHTML = 
+    `
+    <div class="modal-dialog">
+        <div class="modal-header">
+            <h3>${fullName}</h3>
+            <i class="fas fa-times" data-close></i>
+        </div>
+        <div class="modal-body">
+            <div class="modal-img-wrapper">
+                <img src="${url}" alt="${altText}">
+            </div>
+            <p class="description"> ${description}
+            </p>
+        </div
+    </div>
+    `
+    modalContainer.appendChild(parkModal);
+    closeModal();
+}
+
 const modalOpen = (parkId) => {
     const park = findPark(parkId, parks);
-    console.log(park);
+    buildModal(park);
 }
 
 const findParkId = (elm) => {
-    // console.log('find', elm);
     elm.className.includes('park-card')
     ? modalOpen(elm.id)
     : findParkId(elm.parentElement);
-    
-    // ? console.log('has id', elm)
-    // : console.log('does not have id');
 }
 
 
@@ -63,8 +143,6 @@ const handleParkClick = () => {
                 : findParkId(elm);
         })
     })
-    // console.log('here', parks);
-    
 }
 
 const renderDOM = (array) => {
